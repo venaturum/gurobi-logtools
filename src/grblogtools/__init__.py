c9790e59f556e152fbad44ee39225d98380f785f
venaturum <venaturum@gmail.com>
2023-03-03 00:05:27 +1100

    converting to poetry build

--- src/grblogtools/__init__.py
+++ src/grblogtools/__init__.py
@@ -1,4 +1,25 @@
-__version__ = "2.0.0"
-
 from grblogtools.api import get_dataframe, parse
 from grblogtools.plotting import plot
+
+def get_version():
+    def get_version_post_py38():
+        from importlib.metadata import version  # type: ignore
+
+        return version(__name__)
+
+    def get_version_pre_py38():
+        from pkg_resources import get_distribution
+
+        return get_distribution(__name__).version
+
+    def default_version():
+        return "unknown"
+
+    for func in (get_version_post_py38, get_version_pre_py38, default_version):
+        try:
+            return func()
+        except Exception:
+            pass
+
+
+__version__ = get_version()
